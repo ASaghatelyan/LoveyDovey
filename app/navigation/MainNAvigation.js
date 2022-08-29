2cc7e6241301dbc923a07ffddcebca462edc2631
Arman <89474515+ASaghatelyan@users.noreply.github.com>
2022-08-29 17:53:01 +0400

    first commit

---
+++ app/navigation/MainNAvigation.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import StackNavigation from "./StackNavigation";
+import { NavigationContainer } from '@react-navigation/native';
+const Stack = createStackNavigator();
+
+export default function MainNAvigation() {
+  return (
+    <NavigationContainer> 
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="StackNavigation" component={StackNavigation} />
+        {/* <Stack.Screen name="TabNavigation" component={TabNavigation}/> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
